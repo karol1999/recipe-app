502b3f368ac4460551c51ac3b0858c5859e812d6
Karol Matoga <kmat962@gmail.com>
2021-05-21 12:29:46 +0200

    Added controller to manipulate application

--- src/main/java/com/recipeapp/recipeapp/controller/HomeController.java
+++ src/main/java/com/recipeapp/recipeapp/controller/HomeController.java
@@ -1,4 +1,12 @@
 package com.recipeapp.recipeapp.controller;
 
-public class HomeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class  HomeController {
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
 }
